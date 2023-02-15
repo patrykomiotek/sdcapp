76e2f201e2202ca8e4388bebcdc19fc876de7610
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-02-15 12:53:36 +0100

    feat: generator component

--- src/components/Generator/Generator.tsx
+++ src/components/Generator/Generator.tsx
@@ -1,12 +1,28 @@
 import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
 
 import { Text } from '@components/Text';
 
 export const Generator = () => {
+  const [id, setId] = useState(uuidv4());
+  // const [id, setId] = useState<string>();
+  // same as:
+  // const state = useState();
+  // const id = state[0];
+  // const setId = state[1];
+
+  // let id = uuidv4();
+
+  const handleClick = () => {
+    // id = uuidv4();
+    setId(uuidv4());
+    // console.log({ id });
+  }
+
   return (
     <div>
-      <Text>{uuidv4()}</Text>
-      <button>Click me</button>
+      <Text>{id}</Text>
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
