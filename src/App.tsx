ec4c25cb332aff364611ef70f46311790bef2167
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-02-15 14:19:48 +0100

    feat: react router with empty login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,38 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 // import { Button } from '@components/Button';
-import { Button } from '@components/Button';
-import { Text } from '@components/Text';
+import {
+  Routes,
+  Route,
+  BrowserRouter as Router,
+  Link
+} from 'react-router-dom';
+
 import { Generator } from '@components/Generator';
+import { LoginPage } from '@pages/LoginPage';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Generator />
-      <Generator />
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Generator />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
