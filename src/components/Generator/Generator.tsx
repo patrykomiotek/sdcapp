92d53fc436a0441a9414b7c1679a86f5e1383aba
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-02-15 14:01:36 +0100

    feat: project structure

--- src/components/Generator/Generator.tsx
+++ src/components/Generator/Generator.tsx
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
 
-import { Text } from '@components/Text';
+import { Text } from '@atoms/Text';
 
 export const Generator = () => {
   const [id, setId] = useState(uuidv4());
