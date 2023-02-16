550027cf588174f1609a2f228843e8ef0a7cff90
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-02-16 14:47:40 +0100

    feat: context step 5

--- src/components/Auth/AuthContext.tsx
+++ src/components/Auth/AuthContext.tsx
@@ -1,8 +1,48 @@
-import { createContext } from 'react'
+import { useState, createContext } from 'react'
 
 interface User {
   isLoggedIn: boolean;
   username: string;
+}
+
+
+const useAuth = () => {
+  const [state, setState] = useState<User>({ isLoggedIn: false, username: '' });
+
+  const logIn = () => setState({
+    ...state,
+    isLoggedIn: true,
+  });
+
+  const logOut = () => setState({
+    ...state,
+    isLoggedIn: false,
+  });
+
+  const toggle = () => setState({
+    ...state,
+    isLoggedIn: !state.isLoggedIn,
+  });
+
+  const setUsername = (name: string) => setState({
+    ...state,
+    username: name,
+  });
+
+  return { state, logIn, logOut, toggle, setUsername };
+}
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+const AuthProvider = ({ children }: AuthProviderProps) => {
+  // const { state, logIn, logOut, toggle, setUsername } = useAuth();
+  return (
+    <AuthContext.Provider value={useAuth()}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 export const AuthContext = createContext<User | null>(null);
