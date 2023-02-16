f92a23f4cae304ccbb016667cad7d07ebafc3373
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-02-16 18:40:53 +0100

    feat: api intro

---
+++ src/components/Products/ProductsList.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export const ProductsList = () => {
+  const products = useState();
+
+  useEffect(() => {
+    // API request
+    // set state
+  }, []);
+
+  return (
+    <div>
+
+    </div>
+  );
+}
