77fa8ffceb55d621233fedc8b0795f6f63985ca4
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-02-17 15:24:09 +0100

    feat: loan form

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import { Counter } from '../features/counter/Counter';
+import { LoanCase } from '../features/loan/LoanCase';
 
 const HomePage = () => {
   return (
     <div>
       <h1>Home</h1>
       <Counter />
+      <LoanCase />
       {/* <button onClick={() => methodDoesNotExist()}>Break the world</button> */}
       {/* <Text>Ala</Text>
       <Text>Ma</Text>
