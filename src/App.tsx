b4cbfa6d4c54d4d5e371507bf443917643ac05c6
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-02-17 13:54:38 +0100

    feat: redux

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ import {
   QueryClient,
   QueryClientProvider,
 } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools'
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { store } from './store';
+import { Provider as ReduxProvider } from 'react-redux';
 
 
 import { AuthPage } from '@pages/AuthPage';
@@ -30,27 +32,29 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <div className="App">
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider>
-          <AuthProvider>
-            <Router>
-              <Menu />
-              <Routes>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/generator" element={<GeneratorPage />} />
-                <Route path="/auth" element={<AuthPage />} />
-                <Route path="/theme" element={<ThemePage />} />
-                <Route path="/products" element={<ProductsPage />} />
-                <Route path="/products/:id" element={<ProductPage />} />
-                <Route path="/boundary" element={<BoundaryPage />} />
-                <Route path="/viewport" element={<ViewportPage />} />
-                <Route path="/" element={<HomePage />} />
-              </Routes>
-            </Router>
-          </AuthProvider>
-        </ThemeProvider>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
+      <ReduxProvider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider>
+            <AuthProvider>
+              <Router>
+                <Menu />
+                <Routes>
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="/generator" element={<GeneratorPage />} />
+                  <Route path="/auth" element={<AuthPage />} />
+                  <Route path="/theme" element={<ThemePage />} />
+                  <Route path="/products" element={<ProductsPage />} />
+                  <Route path="/products/:id" element={<ProductPage />} />
+                  <Route path="/boundary" element={<BoundaryPage />} />
+                  <Route path="/viewport" element={<ViewportPage />} />
+                  <Route path="/" element={<HomePage />} />
+                </Routes>
+              </Router>
+            </AuthProvider>
+          </ThemeProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </ReduxProvider>
     </div>
   )
 }
